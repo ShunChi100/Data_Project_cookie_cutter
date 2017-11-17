2090a0e7ce4cfc5d0211cf3fcb3230c255809d10
Shun <shunchi100@gmail.com>
2017-11-17 11:42:02 -0800

    Added comments and updated README

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Data_Project_cookie_cutter
-Quick generating a template file and directory structure for data project
+Generating a template file and directory structure for a data project as outlined in the following
+```
+├── LICENSE.md         <- LICENSE
+├── README.md          <- The top-level README for developers using this project.
+├── data/              <- data folder for saving raw data
+│
+├── docs/              <- Documentation folder
+│
+├── img/               <- Image folder.
+│
+├── results/           <- Results folder for saving analysis and results, reports
+│
+├── src/               <- Source code for use in this project.
+│
+├── from_joe/          <- Any unstructured inputs/files/emails.
+```
+
+
+### cookie_cutter_simple.sh
+Quick create the structure with exact structure as shown above.
+Example code:
+```
+bash cookie_cutter_simple.sh root_directory_name
+```
+`root_directory_name` is optional. If not provided, its default name `new_project` is used as the new_project root directory name. This code automatically check if the `root_directory_name` exists in the current directory and give you the option (interactively) to remove the existing one and create a new directory.
+
+Directory _project_cookie_ is created as shown in this repo. [Note: empty sub folders are not pushed to github repo but exist in local root directory.]
+
+### cookie_cutter_interactive.sh
+Interactively create the file and directory structure based on the structure shown above.
+Example code:
+```
+bash cookie_cutter_interactive.sh root_directory_name
+```
+`root_directory_name` is optional. If not provided, its default name `new_project` is used as the new_project root directory name.
+
+There are four interactions when running the script:
+1. If the `root_directory_name` exists in the current directory, you need to choose interactively, whether or not to remove the existing one and create a new directory. If you choose `no`, the script stops and keep the existing directory untouched.
+2. Setting version control. If you choose `yes`, you need to provide the github repo url you want to use for this project.
+3. Adding additional sub-directories. If you choose `yes`, you can add a sub-directory each time (other than provided) and as many times as you want.
+4. Push to github repo. This only shows up when you have set version control (see #2 above). If you choose `yes`, it automatically `add .`, `commit -m "First commit"` and `git push` to remote repo.
+
+[An example project directory is created and push to this repo](https://github.com/ShunChi100/Cookie_Cutter_test.git) [Note: empty sub folders are not pushed to github repo but exist in local root directory.]
--- cookie_cutter_interactive.sh
+++ cookie_cutter_interactive.sh
@@ -1,22 +1,62 @@
 #!\bin\bash
 
+#% DESCRIPTION
+#%    This is cookie cutter for interactively creating a file and folder
+#% structure for a new data science project
+#%
+#% EXAMPLES
+#%    $bash cookie_cutter_interactive root_folder_name
+#% Here the root_folder_name is optional, the default name is *new_project*
+#================================================================
+#- IMPLEMENTATION
+#-    version         $0.0.1
+#-    author          Shun CHI
+#-    copyright       Copyright https://github.com/ShunChi100/Data_Project_cookie_cutter.git
+#-    license         GNU General Public License
+#================================================================
+#  HISTORY
+#     2017/11/16 : Shun: Script creation
+#     2017/11/17 : Shun : Added interactive options for version control, adding more sub folders
+#================================================================
+
+
 # The root folder name: default name 'Project'
 # or user provided name as the first argument for this script
 RootName=${1:-new_project}
 
 # Check if root folder alread in the current folder.
-if [ -d "./$RootName" ]; then
+# if the folder is already existing, interactively choose to keep it
+# or delete it and create a new one
+if [ -d "./$RootName" ];
+then
   echo "The folder './$RootName' exist in the current folder!"
-  echo "Do you want to remove and create new folder [yes/no]:"
+  echo "Do you want to remove it and create a new folder [yes/no]:"
+  echo "(Reomve is PERMANENT, Think Carefully!!!)"
   read input_variable
 
-  # If user input is "yes", then remove the existing root folder
-  if [ $input_variable = "yes" ]; then
-    rm -rf "$RootName"
-  else
-    echo "Fail to create new file structure because the folder already exist!"
-    exit
-  fi
+
+  while true
+  do
+    # If user input is "yes", then remove the existing root folder
+    if [ $input_variable = "yes" ];
+    then
+      rm -rf "$RootName"
+      break
+
+    # If user input is "no", exit the script
+    elif [ $input_variable = "no" ];
+    then
+      echo "Fail to create new file structure because the folder already exist!"
+      exit
+
+    # Ensure user input is either "yes" or "no"
+    else
+      echo "Not valid input. Must be either yes or no. TRY again"
+      echo "Do you want to remove existing folder $RootName and create a new folder [yes/no]:"
+      echo "(Reomve is PERMANENT, Think Carefully!!!)"
+      read input_variable
+    fi
+  done
 fi
 
 # create the project folder
@@ -30,36 +70,41 @@ read version_control_flag
 
 while true
 do
-  if [ $version_control_flag = "yes" ]; then
+  # If user input is "yes", then setting version control and connected to github repo
+  if [ $version_control_flag = "yes" ];
+  then
+    echo "*****************************************************"
+    git init
+    echo " "
+    echo "*****************************************************"
+    echo "Copy your github repo url to here: "
+    read github_url
+    git remote add origin $github_url
+    echo " "
+    echo "*****************************************************"
+    echo "github repo is connected"
+    git remote -v
+    git pull origin master
+
     break
-  elif [ $version_control_flag = "no" ]; then
+
+  # If user input is "no", then skip version control setting
+  elif [ $version_control_flag = "no" ];
+  then
+    echo "Version control hasn't been set!"
     break
+
+  # Ensure user input is either "yes" or "no"
   else
     echo "$version_control_flag"
-    echo "Not valid input. TRY again"
+    echo "Not valid input. Must be either yes or no. TRY again"
     echo "Do you want to set up version control now? [yes/no]"
     read version_control_flag
   fi
 done
 
-if [ $version_control_flag = "yes" ]; then
-  echo "*****************************************************"
-  git init
-  echo " "
-  echo "*****************************************************"
-  echo "Copy your github repo url to here: "
-  read github_url
-  git remote add origin $github_url
-  echo " "
-  echo "*****************************************************"
-  echo "github repo is connected"
-  git remote -v
-  git pull origin master
-else
-  echo "Version control hasn't been set!"
-fi
 
-# create a README.md file and give some basic descriptions
+# create a README.md file and provide some basic guidelines
 touch README.md
 echo -e "# Project Name" > README.md
 echo "description goes here." >> README.md
@@ -72,39 +117,52 @@ echo "Summary of analysis methods and codes go here." >> README.md
 touch LICENSE.md
 echo "Copyright ..." > LICENSE.md
 
-# create directories for data, documentation, source codes, results, and from_joe
+echo " "
+echo "******************************************************"
+echo "README.md and LICENSE.md are created"
+
+# create directories for data, documentation, source codes, results, and img
 mkdir data
 mkdir doc
 mkdir src
 mkdir results
 mkdir img
 
-# Making more sub-folders?
-echo "Add sub-folders other than data, doc, src, results, and img? [yes/no]"
+echo " "
+echo "******************************************************"
+# Interactively creating more sub-folders
+echo "Add sub-folders other than ./data, ./doc, ./src, ./results, and ./img? [yes/no]"
 read more_folder
 
 while true
 do
+  # If yes, add a subfolder according to user's input name
   if [ $more_folder = "yes" ];
   then
     # commit and push
-    echo "sub-folder name (no space, one folder name each time):"
+    echo "sub-folder name (no space in name, one folder name each time):"
     read subfolder_name
     mkdir $subfolder_name
 
     echo "Add more sub-folders [yes/no]"
     read more_folder
 
+  # If no, skip
   elif [ $more_folder = "no" ];
   then
     echo "No more sub-folders added!"
     break
+
+  # Ensure user input is either "yes" or "no"
   else
-    echo "Not valid input. TRY again"
-    echo "sub-folders other than data, doc, src, results, and from_joe? [yes/no]"
+    echo "Not valid input. Must be either yes or no. TRY again"
+    echo "Add sub-folders other than data, doc, src, results, and img? [yes/no]"
     read more_folder
   fi
 done
+echo " "
+echo "******************************************************"
+echo "All subfolders has been created"
 
 
 # push new files to github
@@ -114,26 +172,30 @@ if [ $version_control_flag = "yes" ]; then
   echo "******************************************************"
 
   # read from user choice
-  echo "Do you want to push everything now? [yes/no]"
+  echo "Do you want to push everything to github now? [yes/no]"
   read push_to_github
 
-  # ensure the user input is either yes or no
+
   while true
   do
+    # if yes, add, commit and push to github repository
     if [ $push_to_github = "yes" ];
     then
-      # commit and push
       git add .
       git commit -m "First commit"
       git push origin master
       break
+
+    # if no, skip
     elif [ $push_to_github = "no" ];
     then
       echo "No push to github this time!"
       break
+
+    # ensure the user input is either yes or no
     else
       echo "Not valid input. TRY again"
-      echo "Do you want to push everything now? [yes/no]"
+      echo "Do you want to push everything to github now? [yes/no]"
       read push_to_github
     fi
   done
